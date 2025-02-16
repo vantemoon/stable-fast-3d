84d7eee4a1b05714d44dea8d01281fa4161235b3
Whisper-MMM <sylviassunrx0820@outlook.com>
2025-02-16 22:38:59 +0800

    Update port

--- api.py
+++ api.py
@@ -288,4 +288,4 @@ def requires_bg_remove_endpoint():
 # ------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
