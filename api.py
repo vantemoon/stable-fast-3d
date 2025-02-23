9f622c6fcba11180d1c6d591aefd0181e7113f12
Whisper-MMM <sylviassunrx0820@outlook.com>
2025-02-23 16:20:45 +0800

    Update port forwarding

--- api.py
+++ api.py
@@ -344,4 +344,4 @@ def requires_bg_remove_endpoint():
 # ------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
