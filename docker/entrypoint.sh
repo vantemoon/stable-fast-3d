95ed23ca8f020a28d30925f773faf61f5d0536bc
Whisper-MMM <sylviassunrx0820@outlook.com>
2025-03-17 10:57:14 +0800

    Change number of workers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,4 +7,4 @@ python /.huggingface/hf_login.py
 export USE_CUDA=1
 pip install -v ./texture_baker/
 
-exec gunicorn --workers=4 --bind=0.0.0.0:8000 api:app
+exec gunicorn --workers=1 --bind=0.0.0.0:8000 api:app
